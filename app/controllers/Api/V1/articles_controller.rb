ba5dab5733672cf37c89fa12189fa0b587f5fc91
mkolan <madhukar.rk123@gmail.com>
2019-07-17 19:02:03 -0400

    Add functionality for update

--- app/controllers/Api/V1/articles_controller.rb
+++ app/controllers/Api/V1/articles_controller.rb
@@ -21,6 +21,24 @@ module Api
         end
       end
 
+      def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy
+          render json: {status: 'SUCCESS', message: 'article deleted', data: @article}, status: :ok
+        else
+          render json: {status: 'SUCCESS', message: 'article not deleted', data: @article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        @article = Article.find(params[:id])
+        if @article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message: 'article updated', data: @article}, status: :ok
+        else
+          render json: {status: 'SUCCESS', message: 'article not updated', data: @article.errors}, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def article_params
